dbcd41ef61d003a569472e3bd4474710b74bda8f
stoney <sarkisyanmihail11@gmail.com>
2025-08-23 15:39:34 +0400

    feat(userStory): Wrote locators, Assertion TODO and TabsAndSubtabsTest template class.

--- src/main/java/ge/tbc/testautomation/pages/LocationPage.java
+++ src/main/java/ge/tbc/testautomation/pages/LocationPage.java
@@ -3,11 +3,19 @@ package ge.tbc.testautomation.pages;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.*;
 
 public class LocationPage {
     public final SelenideElement searchBarLocator = $("#tbcx-text-input-1");
+
+    // Tabs / Sub-tabs
+    public final SelenideElement allTabLocator = $x("//button[span[normalize-space(text())='All']]");
+    public final SelenideElement branchesTabLocator = $x("//button[span[normalize-space(text())='Branches']]");
+    public final SelenideElement atmsTabLocator = $x("//button[span[normalize-space(text())='ATMs']]");
+    public final SelenideElement alwaysOpenSubtabLocator = $x("//span[normalize-space(text())='24/7']");
+    public final SelenideElement openNowSubtabLocator = $x("//span[normalize-space(text())='Open now']");
+
+    // Locations(Branches) / Intel about locations(Branches)
     public final ElementsCollection branchesListLocator = $$("app-atm-branches-section-list-item");
 
     public SelenideElement getBranchLocationTitleLocator(SelenideElement location) {
---
+++ src/test/java/ge/tbc/testautomation/tests/TabsAndSubtabsTest.java
@@ -0,0 +1,50 @@
+package ge.tbc.testautomation.tests;
+
+import com.codeborne.selenide.SelenideElement;
+import ge.tbc.testautomation.runners.BaseTest;
+import ge.tbc.testautomation.steps.LocationPageStep;
+import ge.tbc.testautomation.steps.MainPageStep;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ge.tbc.testautomation.data.Constants.*;
+
+public class TabsAndSubtabsTest extends BaseTest {
+    private final MainPageStep mainPageStep = new MainPageStep();
+    private final LocationPageStep locationPageStep = new LocationPageStep();
+
+    @Override
+    protected void openTestPage() {
+        open(TBC_MAIN_URL);
+    }
+
+    @Test(description = "Tabs & sub-tabs (Branch/ATM + services like 24/7, Open now)")
+    public void tabsAndSubTabsTest() {
+        // Saving as list because I need static snapshot and now dynamic wrapper.
+        List<SelenideElement> defaultBranches;
+
+        mainPageStep
+                .rejectAllCookies()
+                .clickQuickActionButton()
+                .clickQuickActionLocationButton();
+
+        defaultBranches = locationPageStep.getBranches().stream().toList();
+
+        /*
+         I need to assert:
+         - Default Tab State
+         - Switch to ATM -> Only ATMS are visible on the menu
+            - click 24/7 subtab and validate that there are only 24/7 ATMS available
+            - click Open now subtab and validate the same
+            - apply both and validate the same thing
+         - Switch to Branches -> Only Branches are visible
+            - click 24/7 subtab and validate that there are only 24/7 Branches available
+            - click Open now subtab and validate the same
+            - apply both and validate the same thing
+
+         - Rest All filters and assert that we've got 'defaultBranches'
+         */
+    }
+}
